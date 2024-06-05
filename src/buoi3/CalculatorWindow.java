15c19444085faaa96c6aa0af415507b908944938
CGM <mrmouse.guru@gmail.com>
2024-06-05 14:15:17 +0700

    updated Label TextField Panel

--- src/buoi3/CalculatorWindow.java
+++ src/buoi3/CalculatorWindow.java
@@ -1,11 +1,31 @@
 package buoi3;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalculatorWindow extends JFrame {
 
+    private JLabel jLabelInputRemote1;
+    private JTextField jTextFieldInputRemote1;
+    private JPanel jPanelRemote;
+
     CalculatorWindow(){
+        
+
+        jLabelInputRemote1 = new JLabel("input1");
+        //add(jLabelInputRemote1);
+        jTextFieldInputRemote1 = new JTextField(10);
+        //add(jTextFieldInputRemote1);
+        jPanelRemote = new JPanel();
+        jPanelRemote.add(jLabelInputRemote1);
+        jPanelRemote.add(jTextFieldInputRemote1);
+
+        add(jPanelRemote);
+
         setSize(400, 400);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
